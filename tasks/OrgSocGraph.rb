ddf4115bf8266a5edb3a754c29c8dcf7f7510b27
Evan Donovan <evan@techmission.org>
2013-08-01 18:20:26 -0400

    Fixed the undefined method in OrgSocGraph

--- tasks/OrgSocGraph.rb
+++ tasks/OrgSocGraph.rb
@@ -7,15 +7,14 @@ module OrgSocGraph
         def name
           :description
         end
-     end.new
+     end.new,
     ]
   }
  
   START_JOBS = [
     Class.new(BaseJob) do
-      # parent job doesn't do anything
       def url
-        "http://www.example.com"
+        'http://www.example.com/'
       end
       
       def get_children(doc)
@@ -25,23 +24,23 @@ module OrgSocGraph
         res = dbh.query('SELECT website_url FROM tbl_organizations WHERE website_url != "" LIMIT 10')
         res.each_row do |r|
           Class.new(BaseJobWithUrl) do
-            def url
-               url
-            end
-                
-            def execute(doc, data_store, fields)
-              # crawl for meta description
-              meta_desc = doc.css("meta[name='description']")
-              if(!meta_desc.empty?)
-               data_store.add_item("orgs.csv", [
-                 self.url,
-                 meta_desc[0]['content']
-               ])
-              end
-            end
-          end.new(r["website_url"])
-        end
+             def url
+                url
+             end
+                     
+             def execute(doc, data_store, fields)
+                meta_desc = doc.css("meta[name='description']")
+                if(!meta_desc.empty?)
+                  data_store.add_item("orgs.csv", [
+                     self.url,
+                     # crawl for meta description
+                    meta_desc[0]['content']
+                  ])
+                end
+             end
+         end.new(r["website_url"])
       end
     end
-  ]
+  end.new
+]
 end
