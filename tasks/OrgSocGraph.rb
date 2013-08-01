54d2ca1815ead3ac0873e740f2f8eedf8990cfb9
Evan Donovan <evan@techmission.org>
2013-08-01 14:08:14 -0400

    Added the organization social graph task.

---
+++ tasks/OrgSocGraph.rb
@@ -0,0 +1,44 @@
+require 'mysql'
+
+module OrgSocGraph
+  FIELDS = {
+    "orgs.csv" => [
+      Class.new(Object) do
+        def name
+          :description
+        end
+     end.new
+    ]
+  }
+ 
+  START_JOBS = [
+    Class.new(BaseJob) do
+      # parent job doesn't do anything
+      def url
+        "http://www.example.com"
+      end
+      
+      def get_children(doc)
+        # do MySQL query to get the URLs of the child jobs
+        dbh = MySQL.real_connect("192.168.0.1", "turb_mi5", "db-fastlane", "techmi5_socgraph")
+        # limit to 10 for test run
+        res = dbh.query('SELECT website_url FROM tbl_organizations WHERE website_url != "" LIMIT 10')
+        res.each_row do |r|
+          Class.new(BaseJobWithUrl) do
+            def url
+               url
+            end
+                
+            def execute(doc, data_store, fields)
+              # crawl for meta description 
+               data_store.add_item("orgs.csv", [
+                 self.url,
+                 doc.css("meta[name='description']").first
+               ])
+            end
+          end.new(r["website_url"])
+        end
+      end
+    end
+  ]
+end
