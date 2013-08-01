434da7b54c21194cc5f8b032d56b5cb619eafb70
Evan Donovan <evan@techmission.org>
2013-08-01 17:39:47 -0400

    Added very minimal example: DMOZ crawler

---
+++ tasks/DMOZ.rb
@@ -0,0 +1,34 @@
+module DMOZ
+  FIELDS = {
+    "dmoz.csv" => [
+      Class.new(Object) do
+        def name
+          :title
+        end
+     end.new,
+      Class.new(Object) do
+        def name
+          :url
+        end
+      end.new
+    ]
+  }
+ 
+  START_JOBS = [
+    Class.new(BaseJob) do
+      def url
+        'http://www.dmoz.org/Arts/Television/Networks/PBS/'
+      end
+      
+      def execute(doc, data_store, fields)
+         url_node = doc.css(".directory-url li a:first")
+       
+         data_store.add_item("dmoz.csv", [
+           self.url,
+           doc.css(".directory-url li a")[0].text,
+           doc.css(".directory-url li a")[0]['href']
+         ])
+      end
+    end.new
+  ]
+end
