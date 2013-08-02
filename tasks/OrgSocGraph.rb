cb72091c6d7a88afa862492e80eeae9f4c0b2d72
Evan Donovan <evan@techmission.org>
2013-08-02 11:34:03 -0400

    Fixed with Jim's help to return children

--- tasks/OrgSocGraph.rb
+++ tasks/OrgSocGraph.rb
@@ -21,14 +21,16 @@ module OrgSocGraph
         # do MySQL query to get the URLs of the child jobs
         dbh = Mysql.real_connect("216.93.247.46", "turb_mi5", "db-fastlane", "techmi5_socgraph")
         # limit to 10 for test run
-        res = dbh.query('SELECT website_url FROM tbl_organizations WHERE website_url != "" LIMIT 10')
-        res.each_hash do |r|
-          Class.new(BaseJobWithURL) do
-             def url
-                url
-             end
-                     
+        res = dbh.query('SELECT website_url FROM tbl_organizations WHERE website_url != ""')
+        children = []
+        while row = res.fetch_hash do
+          children << Class.new(BaseJobWithURL) do         
              def execute(doc, data_store, fields)
+                # handle if the URL can't be opened
+                if doc.nil? 
+                  return
+                end
+                title = doc.css("title").text
                 meta_desc = doc.css("meta[name='description']")
                 if(!meta_desc.empty?)
                   data_store.add_item("orgs.csv", [
@@ -36,10 +38,17 @@ module OrgSocGraph
                      # crawl for meta description
                     meta_desc[0]['content']
                   ])
-                end
+                else
+                  data_store.add_item("orgs.csv", [
+                   self.url,
+                    # just put in the title
+                    title
+                  ])
+                 end
              end
-         end.new(r["website_url"])
+         end.new(row["website_url"]) 
       end
+      children  
     end
   end.new
 ]
